16c2367e52188feee7039d37d08120cefc0813a6
(junu yang)~ <(junu3148@gmail.com)01~>
2023-07-06 12:32:05 +0900

    세팅 커밋 (23/07/06) J

---
+++ src/main/java/com/yes24/controller/Hello.java
@@ -0,0 +1,16 @@
+package com.yes24.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Hello {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		System.out.println("hello");
+		
+		return "index";
+
+	}
+}
